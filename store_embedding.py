b7c27d99dccf60fa7e78512afdc58147fad08195
willyliang <willy_liang@ecomsoft.com.tw>
2024-10-07 11:12:54 +0800

    excersice

---
+++ store_embedding.py
@@ -0,0 +1,14 @@
+from embedding import create_embedding
+from insertdoc import insert_doc 
+
+text = 'aaabbbccc is the framework for building context-aware reasoning applications'
+embedding = create_embedding(text)
+
+doc = {
+    "type": "test_embedding",
+    "name": "test",
+    "content": text,
+    "embeddings": embedding
+}
+
+insert_doc('data', 'test', 'data', doc)
